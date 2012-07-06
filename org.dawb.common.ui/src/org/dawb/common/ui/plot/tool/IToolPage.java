c95eba3d49b8cb194cdd84209cdbcc44428414ac
matthew.gerring <matthew.gerring@gmail.com>
2012-07-06 11:05:30 +0100

    Fix to defect reported by Irakli whereby icons for tool actions in 1D and 2D are different.

--- org.dawb.common.ui/src/org/dawb/common/ui/plot/tool/IToolPage.java
+++ org.dawb.common.ui/src/org/dawb/common/ui/plot/tool/IToolPage.java
@@ -40,8 +40,8 @@ import org.eclipse.ui.part.IPageBookViewPage;
 public interface IToolPage extends IPageBookViewPage, IAdaptable {
 	
 	public enum ToolPageRole {
-		ROLE_2D("org.dawb.common.ui.plot.tool.ROLE_2D", false, true, "icons/plot-tool-2d.png", "Image tools", "Image tools used to profile and inspect images."),  // For instance LineProfile, Profile
-		ROLE_1D("org.dawb.common.ui.plot.tool.ROLE_1D", true, false, "icons/plot-tool-1d.png", "XY plotting", "XY plotting tools"),  // 1D only
+		ROLE_2D("org.dawb.common.ui.plot.tool.ROLE_2D", false, true, "icons/plot-tool-any.png", "Image tools", "Image tools used to profile and inspect images."),  // For instance LineProfile, Profile
+		ROLE_1D("org.dawb.common.ui.plot.tool.ROLE_1D", true, false, "icons/plot-tool-any.png", "XY plotting", "XY plotting tools"),  // 1D only
 		ROLE_1D_AND_2D("org.dawb.common.ui.plot.tool.ROLE_1D_AND_2D", true, true, "icons/plot-tool-any.png", "Plotting tools", "Plotting tools (used both for images and XY plots)"); // Measure, derivative, peak fitting
 		
 		private boolean is1D;
