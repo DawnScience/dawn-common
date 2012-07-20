44c046f28caa1346df06df48a0bc9848473d7e64
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-07-20 17:17:32 +0100

    Fix Jython scripts for DAWNSCI-101

--- org.dawb.workbench.examples/python/python_script1.py
+++ org.dawb.workbench.examples/python/python_script1.py
@@ -1,7 +1,4 @@
-import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset as _ds
 import scisoftpy as dnp
-import scisoftpy.maths as maths
-
 
 # Jython script for manipulating data sets.
 # The variables going into this actor in the workflow are available in the script.
@@ -12,4 +9,4 @@ import scisoftpy.maths as maths
 # It
 
 # Please provide maths of these variables and other available ones in the following lines.
-lnI0It = maths.log(I0/It)
+lnI0It = dnp.log(I0/It)
