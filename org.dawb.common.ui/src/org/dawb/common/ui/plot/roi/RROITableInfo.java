efe910470d888c5edf2348ff60862b221501c2fa
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2012-11-15 17:53:20 +0000

    add pixel values & axes values to the ROITableInfo

--- org.dawb.common.ui/src/org/dawb/common/ui/plot/roi/RROITableInfo.java
+++ org.dawb.common.ui/src/org/dawb/common/ui/plot/roi/RROITableInfo.java
@@ -18,6 +18,7 @@ package org.dawb.common.ui.plot.roi;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 import org.dawb.common.ui.plot.AbstractPlottingSystem;
 import org.dawb.common.ui.plot.region.IROIListener;
@@ -31,11 +32,8 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
@@ -56,88 +54,222 @@ public class RROITableInfo implements IROIListener {
 
 	private Text xStartText;
 	private Text xEndText;
-	private AbstractPlottingSystem plottingSystem;
+	private Text widthText;
+	private Text xBisStartText;
+	private Text xBisEndText;
+	private Text widthBisText;
+	
+	private Text yStartText;
+	private Text yEndText;
+	private Text heightText;
+	private Text yBisStartText;
+	private Text yBisEndText;
+	private Text heightBisText;
+	
 	private Text sumText;
 	private Text maxText;
 	private Text minText;
+	private Text sumBisText;
+	private Text maxBisText;
+	private Text minBisText;
+	
+	private AbstractPlottingSystem plottingSystem;
+	
 	private boolean isProfile;
-	private Button withSum;
-	private boolean showSum = false;
 	private String sumStr = "";
 	private String minStr = "";
 	private String maxStr = "";
 
 	private UpdateJob updateSumMinMax;
 
-	public RROITableInfo(final Composite parent, String labelName, AbstractPlottingSystem plottingSystem, boolean isProfile) {
+	private Composite composite;
 
-		Group roiGroup = new Group(parent, SWT.NONE);
-		roiGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		roiGroup.setLayout(new GridLayout(2, false));
-		roiGroup.setText(labelName);
+	public RROITableInfo(final Composite parent, String labelName, AbstractPlottingSystem plottingSystem, boolean isProfile) {
 		
-		Label xStartLabel = new Label(roiGroup, SWT.NONE);
+		composite = new Group(parent, SWT.NONE);
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		composite.setLayout(new GridLayout(2, false));
+		
+		// x group : xstart, xend, width
+		Group xRoiGroup = new Group (composite, SWT.NONE);
+		xRoiGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		xRoiGroup.setLayout(new GridLayout(2, false));
+		xRoiGroup.setText("Axis Values");
+		
+		Label xStartLabel = new Label(xRoiGroup, SWT.NONE);
 		xStartLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		xStartLabel.setText("x Start");
 		
-		xStartText = new Text(roiGroup, SWT.BORDER);
+		xStartText = new Text(xRoiGroup, SWT.BORDER);
 		xStartText.setEditable(false);
 		xStartText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
-		Label xEndLabel = new Label(roiGroup, SWT.NONE);
+		Label xEndLabel = new Label(xRoiGroup, SWT.NONE);
 		xEndLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		xEndLabel.setText("x End");
 		
-		xEndText = new Text(roiGroup, SWT.BORDER);
+		xEndText = new Text(xRoiGroup, SWT.BORDER);
 		xEndText.setEditable(false);
 		xEndText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
+		Label widthLabel = new Label(xRoiGroup, SWT.NONE);
+		widthLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		widthLabel.setText("Width");
+		
+		widthText = new Text(xRoiGroup, SWT.BORDER);
+		widthText.setEditable(false);
+		widthText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		// x bis group : xstart, xend, width
+		Group xBisRoiGroup = new Group (composite, SWT.NONE);
+		xBisRoiGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		xBisRoiGroup.setLayout(new GridLayout(2, false));
+		xBisRoiGroup.setText("Pixel Values");
+		
+		Label xBisStartLabel = new Label(xBisRoiGroup, SWT.NONE);
+		xBisStartLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		xBisStartLabel.setText("x Start");
+		
+		xBisStartText = new Text(xBisRoiGroup, SWT.BORDER);
+		xBisStartText.setEditable(false);
+		xBisStartText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label xBisEndLabel = new Label(xBisRoiGroup, SWT.NONE);
+		xBisEndLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		xBisEndLabel.setText("x End");
+		
+		xBisEndText = new Text(xBisRoiGroup, SWT.BORDER);
+		xBisEndText.setEditable(false);
+		xBisEndText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label widthBisLabel = new Label(xBisRoiGroup, SWT.NONE);
+		widthBisLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		widthBisLabel.setText("Width");
+		
+		widthBisText = new Text(xBisRoiGroup, SWT.BORDER);
+		widthBisText.setEditable(false);
+		widthBisText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
 		if(!isProfile){
+
+			// y group : ystart, yend, height
+			Group yRoiGroup = new Group (composite, SWT.NONE);
+			yRoiGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			yRoiGroup.setLayout(new GridLayout(2, false));
+			yRoiGroup.setText("Axis Values");
 			
-			withSum = new Button(roiGroup, SWT.CHECK);
-			withSum.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-			withSum.setText("Show");
-			withSum.setSelection(false);
-			withSum.addSelectionListener(new SelectionListener() {
-				@Override
-				public void widgetSelected(SelectionEvent e) {
-					if(withSum.getSelection())
-						showSum = true;
-					else
-						showSum = false;
-				}
-				
-				@Override
-				public void widgetDefaultSelected(SelectionEvent e) {
-					widgetSelected(e);
-				}
-			});
-			Composite sumComposite = new Composite(roiGroup, SWT.NONE);
-			sumComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-			sumComposite.setLayout(new GridLayout(2, false));
-			Label sumLabel = new Label(sumComposite, SWT.NONE);
+			Label yStartLabel = new Label(yRoiGroup, SWT.NONE);
+			yStartLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			yStartLabel.setText("y Start");
+			
+			yStartText = new Text(yRoiGroup, SWT.BORDER);
+			yStartText.setEditable(false);
+			yStartText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label yEndLabel = new Label(yRoiGroup, SWT.NONE);
+			yEndLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			yEndLabel.setText("y End");
+			
+			yEndText = new Text(yRoiGroup, SWT.BORDER);
+			yEndText.setEditable(false);
+			yEndText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label heightLabel = new Label(yRoiGroup, SWT.NONE);
+			heightLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			heightLabel.setText("Height");
+			
+			heightText = new Text(yRoiGroup, SWT.BORDER);
+			heightText.setEditable(false);
+			heightText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			// y bis group : ystart, yend, height
+			Group yBisRoiGroup = new Group (composite, SWT.NONE);
+			yBisRoiGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			yBisRoiGroup.setLayout(new GridLayout(2, false));
+			yBisRoiGroup.setText("Pixel Values");
+			
+			Label yBisStartLabel = new Label(yBisRoiGroup, SWT.NONE);
+			yBisStartLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			yBisStartLabel.setText("y Start");
+			
+			yBisStartText = new Text(yBisRoiGroup, SWT.BORDER);
+			yBisStartText.setEditable(false);
+			yBisStartText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label yBisEndLabel = new Label(yBisRoiGroup, SWT.NONE);
+			yBisEndLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			yBisEndLabel.setText("y End");
+			
+			yBisEndText = new Text(yBisRoiGroup, SWT.BORDER);
+			yBisEndText.setEditable(false);
+			yBisEndText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label heightBisLabel = new Label(yBisRoiGroup, SWT.NONE);
+			heightBisLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			heightBisLabel.setText("Height");
+			
+			heightBisText = new Text(yBisRoiGroup, SWT.BORDER);
+			heightBisText.setEditable(false);
+			heightBisText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+			// sumMinMax group
+			Group sumMinMaxGroup = new Group (composite, SWT.NONE);
+			sumMinMaxGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			sumMinMaxGroup.setLayout(new GridLayout(2, false));
+			
+			Label sumLabel = new Label(sumMinMaxGroup, SWT.NONE);
 			sumLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 			sumLabel.setText("Sum");
 			
-			sumText = new Text(sumComposite, SWT.BORDER);
+			sumText = new Text(sumMinMaxGroup, SWT.BORDER);
 			sumText.setEditable(false);
 			sumText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 			
-			Label maxLabel = new Label(roiGroup, SWT.NONE);
-			maxLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-			maxLabel.setText("Min");
+			Label minLabel = new Label(sumMinMaxGroup, SWT.NONE);
+			minLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			minLabel.setText("Min");
 			
-			maxText = new Text(roiGroup, SWT.BORDER);
+			minText = new Text(sumMinMaxGroup, SWT.BORDER);
+			minText.setEditable(false);
+			minText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label maxLabel = new Label(sumMinMaxGroup, SWT.NONE);
+			maxLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			maxLabel.setText("Max");
+			
+			maxText = new Text(sumMinMaxGroup, SWT.BORDER);
 			maxText.setEditable(false);
 			maxText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 			
-			Label minLabel = new Label(roiGroup, SWT.NONE);
-			minLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-			minLabel.setText("Max");
+			// bis sumMinMax group
+			Group sumMinMaxBisGroup = new Group (composite, SWT.NONE);
+			sumMinMaxBisGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			sumMinMaxBisGroup.setLayout(new GridLayout(2, false));
 			
-			minText = new Text(roiGroup, SWT.BORDER);
-			minText.setEditable(false);
-			minText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			Label sumBisLabel = new Label(sumMinMaxBisGroup, SWT.NONE);
+			sumBisLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			sumBisLabel.setText("Sum");
+			
+			sumBisText = new Text(sumMinMaxBisGroup, SWT.BORDER);
+			sumBisText.setEditable(false);
+			sumBisText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label minBisLabel = new Label(sumMinMaxBisGroup, SWT.NONE);
+			minBisLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			minBisLabel.setText("Min");
+			
+			minBisText = new Text(sumMinMaxBisGroup, SWT.BORDER);
+			minBisText.setEditable(false);
+			minBisText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
+			Label maxBisLabel = new Label(sumMinMaxBisGroup, SWT.NONE);
+			maxBisLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+			maxBisLabel.setText("Max");
+			
+			maxBisText = new Text(sumMinMaxBisGroup, SWT.BORDER);
+			maxBisText.setEditable(false);
+			maxBisText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+			
 		}
 		this.isProfile = isProfile;
 		this.plottingSystem = plottingSystem;
@@ -149,27 +281,71 @@ public class RROITableInfo implements IROIListener {
 	public void dispose(){
 		this.plottingSystem.removeRegionListener(regionListener);
 	}
+	
+	public Composite getComposite(){
+		return this.composite;
+	}
 
 	private void setEditingRegion(final RectangularROI rroi) {
 		double xStart = rroi.getPointX();
-	//	double yStart = rroi.getPointY();
+		double yStart = rroi.getPointY();
 		double xEnd = rroi.getEndPoint()[0];
-	//	double yEnd = rroi.getEndPoint()[1];
-		xStartText.setText(String.valueOf(xStart));
-		xEndText.setText(String.valueOf(xEnd));
+		double yEnd = rroi.getEndPoint()[1];
+		
+		// main plotting system
 		if(!isProfile){
-			int xStartPt = (int) rroi.getPoint()[0];
-			int yStartPt = (int) rroi.getPoint()[1];
-			int xStopPt = (int) rroi.getEndPoint()[0];
-			int yStopPt = (int) rroi.getEndPoint()[1];
-			int xInc = rroi.getPoint()[0]<rroi.getEndPoint()[0] ? 1 : -1;
-			int yInc = rroi.getPoint()[1]<rroi.getEndPoint()[1] ? 1 : -1;
-			
-			updateSumMinMax.update(plottingSystem, xStartPt, xStopPt, yStartPt, yStopPt, xInc, yInc);
+			try{
+				// We get the axes data to convert from the axis pixel to data values
+				Collection<ITrace> traces = plottingSystem.getTraces();
+				Iterator<ITrace> it = traces.iterator();
+				while(it.hasNext()){
+					ITrace trace = it.next();
+					if(trace instanceof IImageTrace){
+						IImageTrace image = (IImageTrace)trace;
+						List<AbstractDataset> axes = image.getAxes();
+						// x axis and width
+						double xNewStart = axes.get(0).getElementDoubleAbs((int)Math.round(xStart));
+						double xNewEnd =axes.get(0).getElementDoubleAbs((int)(int)Math.round(xEnd));
+						xStartText.setText(String.valueOf(xNewStart));
+						xEndText.setText(String.valueOf(xNewEnd));
+						widthText.setText(String.valueOf(xNewEnd-xNewStart));
+						// yaxis and height
+						double yNewStart = axes.get(1).getElementDoubleAbs((int)Math.round(yStart));
+						double yNewEnd =axes.get(1).getElementDoubleAbs((int)(int)Math.round(yEnd));
+						yStartText.setText(String.valueOf(yNewStart));
+						yEndText.setText(String.valueOf(yNewEnd));
+						heightText.setText(String.valueOf(yNewEnd-yNewStart));
+					}
+				}
+				xBisStartText.setText(String.valueOf(xStart));
+				xBisEndText.setText(String.valueOf(xEnd));
+				widthBisText.setText(String.valueOf(xEnd-xStart));
+				
+				yBisStartText.setText(String.valueOf(yStart));
+				yBisEndText.setText(String.valueOf(yEnd));
+				heightBisText.setText(String.valueOf(yEnd-yStart));
+				
+				int xStartPt = (int) rroi.getPoint()[0];
+				int yStartPt = (int) rroi.getPoint()[1];
+				int xStopPt = (int) rroi.getEndPoint()[0];
+				int yStopPt = (int) rroi.getEndPoint()[1];
+				int xInc = rroi.getPoint()[0]<rroi.getEndPoint()[0] ? 1 : -1;
+				int yInc = rroi.getPoint()[1]<rroi.getEndPoint()[1] ? 1 : -1;
+				
+				updateSumMinMax.update(plottingSystem, xStartPt, xStopPt, yStartPt, yStopPt, xInc, yInc);
 
-			sumText.setText(sumStr);
-			minText.setText(minStr);
-			maxText.setText(maxStr);
+				sumText.setText(sumStr);
+				minText.setText(minStr);
+				maxText.setText(maxStr);
+			} catch (Exception e) {
+				logger.debug("Error while updating the ROITableInfo:"+ e);
+			}
+		} 
+		//if profile plotting system: no need to convert as the axes are already correctly set
+		else {
+			xStartText.setText(String.valueOf(xStart));
+			xEndText.setText(String.valueOf(xEnd));
+			widthText.setText(String.valueOf(xEnd-xStart));
 		}
 	}
 
@@ -226,8 +402,7 @@ public class RROITableInfo implements IROIListener {
 						logger.debug("Error getting region data:"+ e);
 					}
 					
-					if(showSum)
-						sumStr = dataRegion.sum(true).toString();
+					sumStr = dataRegion.sum(true).toString();
 					if (monitor.isCanceled()) return Status.CANCEL_STATUS;
 					minStr = dataRegion.min().toString();
 					maxStr = dataRegion.max().toString();
