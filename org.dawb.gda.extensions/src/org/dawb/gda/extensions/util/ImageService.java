393f152481bfcd81521379954c25950ee55c1274
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-12-19 11:02:02 +0000

    Removed unused import

--- org.dawb.gda.extensions/src/org/dawb/gda/extensions/util/ImageService.java
+++ org.dawb.gda.extensions/src/org/dawb/gda/extensions/util/ImageService.java
@@ -14,7 +14,6 @@ import org.dawb.common.services.IPaletteService;
 import org.dawb.common.services.ImageServiceBean;
 import org.dawb.common.services.ImageServiceBean.HistoType;
 import org.dawb.common.services.ImageServiceBean.ImageOrigin;
-import org.dawb.common.ui.image.PaletteFactory;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.PaletteData;
