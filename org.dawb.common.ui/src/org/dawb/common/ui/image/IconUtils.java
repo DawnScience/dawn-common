f17724530dbb870cb1428f02b5926668d01f0292
matthew.gerring <matthew.gerring@gmail.com>
2013-01-25 17:27:11 +0000

    Made changes to colours.

--- org.dawb.common.ui/src/org/dawb/common/ui/image/IconUtils.java
+++ org.dawb.common.ui/src/org/dawb/common/ui/image/IconUtils.java
@@ -1,21 +1,30 @@
 package org.dawb.common.ui.image;
 
+import org.dawb.common.ui.image.IconUtils.ShapeType;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.draw2d.ColorConstants;
 
 public class IconUtils {
+	
+	public enum ShapeType {
+		SQUARE, TRIANGLE, CIRCLE;
+	}
 
 	public static ImageDescriptor createIconDescriptor(String iconText) {
 
-		final ImageData data  = new ImageData(16, 16, 8, PaletteFactory.makeGrayScalePalette());
+		final ImageData data  = new ImageData(16, 16, 8, makeGrayScalePalette());
         final Image     image = new Image(Display.getCurrent(), data);
         
         final GC gc = new GC(image);
@@ -34,6 +43,76 @@ public class IconUtils {
 				return image.getImageData();
 			}
 		};
+	}
+
+
+	public static ImageDescriptor createPenDescriptor(int penSize) {
+
+		final ImageData data  = new ImageData(16, 16, 8, makeGrayScalePalette());
+        final Image     image = new Image(Display.getCurrent(), data);
+        
+        final GC gc = new GC(image);
+       
+        gc.setBackground(Display.getDefault().getActiveShell().getBackground());
+        gc.fillRectangle(new Rectangle(0,0,16,16));
+        gc.setBackground(ColorConstants.gray);
+        gc.fillRectangle(2, 2, penSize, penSize);
+        
+        gc.dispose();
+		
+		return new ImageDescriptor() {			
+			@Override
+			public ImageData getImageData() {
+				return image.getImageData();
+			}
+		};
+	}
+
+	public static ImageDescriptor getBrushIcon(int size, ShapeType shape) {
+		
+		final ImageData data  = new ImageData(16, 16, 8, makeGrayScalePalette());
+        final Image     image = new Image(Display.getCurrent(), data);
+        
+        final GC gc = new GC(image);
+       
+        gc.setBackground(Display.getDefault().getActiveShell().getBackground());
+        gc.fillRectangle(new Rectangle(0,0,16,16));
+        gc.setForeground(ColorConstants.gray);
+        gc.setLineWidth(2);
+        switch (shape) {
+        case SQUARE:
+            gc.drawRectangle(2,2,size,size);
+            break;
+        case TRIANGLE:
+        	final PointList pl = new PointList();
+        	pl.addPoint(2+(size/2), 2);
+           	pl.addPoint(2+size, 2+size);
+           	pl.addPoint(2, 2+size);
+            gc.drawPolygon(pl.toIntArray());
+            break;
+        case CIRCLE:
+            gc.drawOval(2,2,size,size);
+            break;
+       }
+            
+        
+        gc.dispose();
+		
+		return new ImageDescriptor() {			
+			@Override
+			public ImageData getImageData() {
+				return image.getImageData();
+			}
+		};
+	}
+
+	
+	public static PaletteData makeGrayScalePalette() {
+		RGB grayscale[] = new RGB[256];
+		for (int i = 0; i < 256; i++) {
+			grayscale[i] = new RGB(i, i, i);
+		}
+		return new PaletteData(grayscale);
 	}
 
 }
