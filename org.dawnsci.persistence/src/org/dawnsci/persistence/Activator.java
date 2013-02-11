da473823655940db0cb41db16275e61eee9eb73b
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2013-02-11 15:48:13 +0000

    remove system.out.println

--- org.dawnsci.persistence/src/org/dawnsci/persistence/Activator.java
+++ org.dawnsci.persistence/src/org/dawnsci/persistence/Activator.java
@@ -6,12 +6,15 @@ import org.dawb.common.services.IPersistenceService;
 import org.dawnsci.persistence.internal.PersistenceServiceImpl;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
 
+	Logger logger = LoggerFactory.getLogger(Activator.class);
+
 	public void start(BundleContext context) throws Exception {
-		// What's wrong with loggers?
-		System.out.println("Starting org.dawnsci.persistence");
+		logger.info("Starting org.dawnsci.persistence");
 		Hashtable<String, String> props = new Hashtable<String, String>(1);
 		props.put("description", "A service used to save and/or load data to hdf5 files");
 		context.registerService(IPersistenceService.class, new PersistenceServiceImpl(), props);
@@ -19,7 +22,7 @@ public class Activator implements BundleActivator {
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		System.out.println("Stopping org.dawnsci.persistence");
+		logger.info("Stopping org.dawnsci.persistence");
 	}
 
 }
