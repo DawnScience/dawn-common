2e71f3eacdb2a7b7779b74a5f9ee1d50c7a19df7
matthew.gerring <matthew.gerring@gmail.com>
2013-03-13 14:38:26 +0000

    Fix to http://jira.diamond.ac.uk/browse/SCI-712. Extracted ncsa.hdf from org.dawb.hdf5

--- org.dawb.hdf5/src/ncsa/hdf/hdf5lib/callbacks/H5D_iterate_t.java
+++
@@ -1,7 +0,0 @@
-package ncsa.hdf.hdf5lib.callbacks;
-
-public interface H5D_iterate_t {
-/**    public ArrayList iterdata = new ArrayList();
-  * Any derived interfaces must define the single public varaible as above.
-  */
-}
