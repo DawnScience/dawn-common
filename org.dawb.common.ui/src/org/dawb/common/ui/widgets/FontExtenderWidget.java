8e8766c6db7ffe4ff7ca23950a0add3955cd28d8
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2013-04-05 12:43:25 +0100

    FontExtenderWidget: delete buttons and make the text display depend on an autoresize flag

--- org.dawb.common.ui/src/org/dawb/common/ui/widgets/FontExtenderWidget.java
+++ org.dawb.common.ui/src/org/dawb/common/ui/widgets/FontExtenderWidget.java
@@ -1,28 +1,22 @@
 package org.dawb.common.ui.widgets;
 
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.dnd.Clipboard;
-import org.eclipse.swt.dnd.TextTransfer;
-import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Canvas;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.FontDialog;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.RowLayout;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 
 /**
- * Widget that creates a canvas and a Font Dialog button to display text in various Fonts and sizes
+ * Widget that creates a canvas to display a text given a specific font
  * @author wqk87977
  *
  */
@@ -31,6 +25,8 @@ public class FontExtenderWidget extends Composite {
 	private Font font;
 	private String fontText;
 	private Canvas canvas;
+	private boolean isAutoResize = false;
+
 	/**
 	 * Create the composite.
 	 * @param parent
@@ -40,99 +36,62 @@ public class FontExtenderWidget extends Composite {
 	 */
 	public FontExtenderWidget(final Composite parent, int style, String title) {
 		super(parent, style);
-		
 		font = new Font(parent.getDisplay(), "Helvetica", 30, SWT.BOLD);
 		fontText = "";
-		final Clipboard cb = new Clipboard(parent.getDisplay());
-//		setLayout(new RowLayout(SWT.VERTICAL));
-		setLayout(new GridLayout(1, false));
-		
-		final Composite menuComposite = new Composite(this, SWT.TOP);
-		RowLayout menuLayout = new RowLayout();
-		menuLayout.wrap=true;
-		menuLayout.center=true;
-		menuComposite.setLayout(menuLayout);
-		
-		Label titleLabel = new Label(menuComposite, SWT.NONE);
-		titleLabel.setText(title);
-		
-		Button button = new Button(menuComposite, SWT.PUSH);
-		button.setText("Font...");
-		button.setToolTipText("Change display size");
-		button.addSelectionListener(new SelectionAdapter() {
+		GridData gd = new GridData(GridData.FILL_BOTH);
+		setLayoutData(gd);
+		GridLayout gl = new GridLayout();
+		setLayout(gl);
 
-			@Override
-			public void widgetSelected(SelectionEvent event) {
-
-				FontDialog dlg = new FontDialog(parent.getShell());
-
-				FontData fontData = dlg.open();
-				if (fontData != null) {
-					if (font != null)
-						font.dispose();
-					font = new Font(parent.getShell().getDisplay(), fontData);
-					
-					canvas.redraw();
-				}
-			}
-		});
-		
-		Button copyButton = new Button(menuComposite, SWT.PUSH);
-		copyButton.setText("Copy");
-		copyButton.setToolTipText("Copy to clipboard");
-		copyButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent event) {
-				TextTransfer textTransfer = TextTransfer.getInstance();
-				cb.setContents(new Object[] { fontText },
-						new Transfer[] { textTransfer });
-			}
-		});
-		
 		ScrolledComposite scrolledComposite = new ScrolledComposite(this, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
-//		scrolledComposite.setLayout(new GridLayout());
 		scrolledComposite.setExpandHorizontal(true);
 		scrolledComposite.setExpandVertical(true);
 
-		
-		canvas = new Canvas(scrolledComposite, SWT.BORDER);
-		canvas.setBackground(new Color(parent.getDisplay(), 255, 255, 255));
+		canvas = new Canvas(scrolledComposite, SWT.BORDER | SWT.FILL);
+		canvas.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+		FormData fd = new FormData();
+		fd.left = new FormAttachment(0, 5);
+		fd.right = new FormAttachment(100, -5);
+		fd.top = new FormAttachment(0, 5);
+		fd.bottom = new FormAttachment(100, -35);
+		canvas.setLayoutData(fd);
 		canvas.setToolTipText("Shows the "+title);
-//		canvas.setLayout(new GridLayout());
 		GridData gridData = new GridData(GridData.FILL_BOTH);
 		gridData.horizontalSpan = 9;
+
 		canvas.setLayoutData(gridData);
 		canvas.addPaintListener(new PaintListener() {
 			@Override
 			public void paintControl(PaintEvent event) {
-				// Set the font into the gc
+				if(isAutoResize){
+					FontData[] fontData = font.getFontData();
+					// Figure out how big our drawing area is
+					Rectangle rect = canvas.getBounds();
+					int width = rect.width;
+					// Get the width of each character
+					int fontWidth = event.gc.getFontMetrics().getAverageCharWidth();
+					// Get the current Font height
+					int fontHeight = event.gc.getFontMetrics().getHeight();
+					//Calculate the fontHeight/fontWidth ratio
+					double ratio = fontHeight / fontWidth;
+					//Calculate the new height
+					int height = (int)((width * ratio) / 20);
+					// Reset the font with new font calculated height
+					font.dispose();
+					font = new Font(parent.getShell().getDisplay(), fontData[0].getName(), height, fontData[0].getStyle());
+				}
 				event.gc.setFont(font);
 				event.gc.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_WHITE));
-				//event.gc.setForeground(color);
-				// Calcalute the width (nad height) of the string
-				//Point pt = event.gc.stringExtent(fontText);
-
-				// Figure out how big our drawing area is
-//				Rectangle rect = canvas.getBounds();
-
-				// Calculate the height of the font
-//				int height = event.gc.getFontMetrics().getHeight();
-//				int middleX = rect.height/3 - height;
-//				int middleY = rect.width/3;
-				event.gc.drawString(fontText, 0, 0);
-
+				event.gc.drawText(fontText, 0, 0);
 			}
 		});
 
 		canvas.setSize(600, 100);
 		
 		scrolledComposite.setContent(canvas);
-		scrolledComposite.setMinHeight(100);
-		scrolledComposite.setMinWidth(600);
-		
-		
-
+//		scrolledComposite.setMinHeight(100);
+//		scrolledComposite.setMinWidth(300);
 	}
 
 	@Override
@@ -147,5 +106,24 @@ public class FontExtenderWidget extends Composite {
 	public void update(String text){
 		fontText = text;
 		canvas.redraw();
+	}
+
+	/**
+	 * Returns the Font
+	 */
+	public Font getFont(){
+		return font;
+	}
+
+	public void setFont(Font font){
+		this.font = font;
+	}
+
+	public String getText(){
+		return fontText;
+	}
+
+	public void setAutoResize(boolean b) {
+		this.isAutoResize  = b;
 	}
 }
