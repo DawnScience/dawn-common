b4d1d996e2b35d23202396698cba978bde24797e
Jacob Filik <jacob.filik@diamond.ac.uk>
2013-04-19 10:24:22 +0100

    Logging and service registration added to the jexl Activator

--- org.dawnsci.jexl/src/org/dawnsci/jexl/Activator.java
+++ org.dawnsci.jexl/src/org/dawnsci/jexl/Activator.java
@@ -1,11 +1,19 @@
 package org.dawnsci.jexl;
 
+import java.util.Hashtable;
+
+import org.dawb.common.services.expressions.IExpressionService;
+import org.dawnsci.jexl.internal.ExpressionServiceImpl;
+import org.dawnsci.jexl.Activator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	Logger logger = LoggerFactory.getLogger(Activator.class);
 
 	static BundleContext getContext() {
 		return context;
@@ -15,8 +23,13 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+	public void start(BundleContext context) throws Exception {
+		logger.info("Starting org.dawnsci.jexl");
+		Hashtable<String, String> props = new Hashtable<String, String>(1);
+		props.put("description", "A service used to create an expression engine");
+		context.registerService(IExpressionService.class, new ExpressionServiceImpl(), props);
+		Activator.context = context;
+		
 	}
 
 	/*
@@ -24,7 +37,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
+		logger.info("Stopping org.dawnsci.jexl");
 		Activator.context = null;
 	}
-
 }
