ef4d50b960a516e40fdf6ed4ded26782970db2c9
matthew.gerring <matthew.gerring@gmail.com>
2013-04-22 10:56:19 +0100

    Finished move, now starting on refactor.

--- org.dawnsci.conversion.ui/src/org/dawnsci/conversion/ui/Activator.java
+++ org.dawnsci.conversion.ui/src/org/dawnsci/conversion/ui/Activator.java
@@ -20,7 +20,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.dawb.workbench.convert";
+	public static final String PLUGIN_ID = "org.dawnsci.conversion.ui";
 
 	// The shared instance
 	private static Activator plugin;
