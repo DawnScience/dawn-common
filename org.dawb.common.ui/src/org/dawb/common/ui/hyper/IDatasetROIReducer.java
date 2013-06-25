4b8919ba8e5e6023a957fe0591eeea4047e535d2
Jacob Filik <jacob.filik@diamond.ac.uk>
2013-06-25 14:00:28 +0100

    Update to HyperWindow to work for ND arrays where N > 3

--- org.dawb.common.ui/src/org/dawb/common/ui/hyper/IDatasetROIReducer.java
+++ org.dawb.common.ui/src/org/dawb/common/ui/hyper/IDatasetROIReducer.java
@@ -4,21 +4,25 @@ import java.util.List;
 
 import org.dawnsci.plotting.api.region.IRegion.RegionType;
 
+import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.IDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.ILazyDataset;
+import uk.ac.diamond.scisoft.analysis.dataset.Slice;
 import uk.ac.diamond.scisoft.analysis.roi.IROI;
 
 public interface IDatasetROIReducer {
+
 	
-	public boolean isOutput1D();
+public boolean isOutput1D();
 	
-	public IDataset reduce(ILazyDataset data, List<ILazyDataset> axes, int dim, IROI roi);
+	public IDataset reduce(ILazyDataset data, List<AbstractDataset> axes, IROI roi, Slice[] slices, int[] order);
 	
 	public List<RegionType> getSupportedRegionType();
 	
-	public IROI getInitialROI(List<ILazyDataset> axes, int dim);
+	public IROI getInitialROI(List<AbstractDataset> axes, int[] order);
 	
 	public boolean supportsMultipleRegions();
 	
 	public List<IDataset> getAxes();
+	
 }
