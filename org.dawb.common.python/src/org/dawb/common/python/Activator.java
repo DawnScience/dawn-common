3939208a5cb2c65977b442cf17ebac8db8bdde06
Jonah Graham <jonah@kichwacoders.com>
2013-08-11 23:31:01 +0100

    Fix PLUGIN_ID in Activator

--- org.dawb.common.python/src/org/dawb/common/python/Activator.java
+++ org.dawb.common.python/src/org/dawb/common/python/Activator.java
@@ -18,7 +18,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.dawb.python"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.dawb.common.python"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
