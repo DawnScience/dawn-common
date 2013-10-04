4afe9302ccd9e2c14aae085ee4a772811995a7e3
matthew.gerring <matthew.gerring@gmail.com>
2013-10-04 11:00:44 +0100

    Fixed converter for datasets that are not equal in size.

--- org.dawnsci.conversion/src/org/dawnsci/conversion/converters/CompareConverter.java
+++ org.dawnsci.conversion/src/org/dawnsci/conversion/converters/CompareConverter.java
@@ -17,6 +17,7 @@ import org.dawb.hdf5.Nexus;
 import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.DatasetUtils;
 import uk.ac.diamond.scisoft.analysis.dataset.IDataset;
+import uk.ac.diamond.scisoft.analysis.dataset.IndexIterator;
 
 /**
  * This converter creates stacks from 
@@ -68,16 +69,26 @@ public class CompareConverter extends AbstractConversion{
 	}
 
 
+	private Map<String, int[]> requiredShapes;
+	
  	@Override
 	protected void convert(IDataset slice) throws Exception {
 		
- 		final String datasetPath = slice.getName(); // Slice mus tbe named the same as the path it will write to
+ 		final String datasetPath = slice.getName(); // Slice must be named the same as the path it will write to
  		final Datatype        dt = getDatatype(slice);
 		
  		final String name = datasetPath.substring(datasetPath.lastIndexOf('/')+1);
  		final Group group = groups.get(datasetPath);
  		
- 		AbstractDataset abs = DatasetUtils.convertToAbstractDataset(slice);
+ 		
+ 		AbstractDataset abs = DatasetUtils.convertToAbstractDataset(slice).squeeze();
+ 		
+ 		// Each dataset must come through as the same shape, even if it is not.
+ 		// We keep the requiredShapes and force the current dataset to be the same
+ 		// as previous shapes.
+ 		int[] requiredShape = getRequiredShape(datasetPath, abs.getShape());
+ 		abs = resize(abs, requiredShape);
+
 		Dataset d = hFile.appendDataset(name, dt, getLong(slice.getShape()), abs.getBuffer(), group);
 		if (!written.get(datasetPath)) {
 			hFile.setNexusAttribute(d, Nexus.SDS);
@@ -90,10 +101,78 @@ public class CompareConverter extends AbstractConversion{
 			throw new Exception("Conversion is cancelled!");
 		}
 	}
+ 	
+	public static AbstractDataset resize(final AbstractDataset a, final int... shape) {
+		int size = a.getSize();
+		AbstractDataset rdata = AbstractDataset.zeros(a.getElementsPerItem(), shape, a.getDtype());
+		IndexIterator it = rdata.getIterator();
+		while (it.hasNext()) {
+			rdata.setObjectAbs(it.index, it.index<size ? a.getObjectAbs(it.index) : Double.NaN);
+		}
+
+		return rdata;
+	}
+
 	
+ 	private int[] getRequiredShape(String path, int[] defaultShape) {
+ 		if (requiredShapes==null) requiredShapes = new HashMap<String, int[]>();
+ 		int[] shape = context.getUserObject()!=null ? ((ConversionInfoBean)context.getUserObject()).getRequiredShape(path) : null;
+ 		if (shape==null) shape = requiredShapes.get(path);
+ 		if (shape==null) {
+ 			shape = defaultShape;
+ 			requiredShapes.put(path, defaultShape);
+ 		}
+ 		return shape;
+ 	}
 	
 	public void close(IConversionContext context) throws Exception{
 		hFile.close();
 	}
 
+	
+	/**
+	 * To be used as the user object to convey data about the conversion.
+	 * @author fcp94556
+	 *
+	 */
+	public static final class ConversionInfoBean {
+
+		private Map<String, int[]> requiredShapes = new HashMap<String, int[]>();
+
+	 	private int[] getRequiredShape(String path) {
+	 		if (requiredShapes==null) return null;
+			return requiredShapes.get(path);
+		}
+	 	private void addRequiredShape(String path, int[] shape) {
+			requiredShapes.put(path, shape);
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime
+					* result
+					+ ((requiredShapes == null) ? 0 : requiredShapes.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ConversionInfoBean other = (ConversionInfoBean) obj;
+			if (requiredShapes == null) {
+				if (other.requiredShapes != null)
+					return false;
+			} else if (!requiredShapes.equals(other.requiredShapes))
+				return false;
+			return true;
+		}
+		
+	}
 }
