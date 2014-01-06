1d507f5d1d9fe1007b696ed913868b220869e95e
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-01-06 13:09:32 +0000

    Moml updater: add a static method that updates the XML tree by removing the unnecessary expression mode node

--- org.dawnsci.persistence/src/org/dawnsci/persistence/workflow/xml/MomlUpdater.java
+++ org.dawnsci.persistence/src/org/dawnsci/persistence/workflow/xml/MomlUpdater.java
@@ -2,12 +2,6 @@ package org.dawnsci.persistence.workflow.xml;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,11 +9,13 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
 import org.xml.sax.SAXException;
 
 /**
@@ -31,97 +27,72 @@ import org.xml.sax.SAXException;
  */
 public class MomlUpdater {
 
-	private final Logger Logger = LoggerFactory.getLogger(MomlUpdater.class);
-	private String filePath;
-
-	public MomlUpdater() {
-//		this.filePath = filePath;
-	}
+	private static final Logger Logger = LoggerFactory.getLogger(MomlUpdater.class);
+	private static final String EXPRESSION_MODE = "Expression Mode";
 
 	/**
-	 * Updates the Moml file after reading its workbench version
-	 * and deleting its expression mode properties if necessary.
+	 * Updates the Moml file by deleting its expression mode properties if necessary.
 	 * @param filePath
 	 *          The full file Path of the Moml file
+	 * @return String
+	 *          The updated moml file as a string
 	 */
-	public void updateMoml(String filePath) {
-//		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//		factory.setValidating(false);
-//		factory.setIgnoringElementContentWhitespace(true);
+	public static String updateMoml(String filePath) {
 		try {
-//			DocumentBuilder builder = factory.newDocumentBuilder();
-//			File file = new File(filePath);
-//			String xml = readFile(filePath, StandardCharsets.UTF_8);
+			File fXmlFile = new File(filePath);
 
-//			Document doc = builder.parse(new InputSource(new StringReader(xml)));
-			
-			File fXmlFile = new File(filePath);  
-
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();  
-			  
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 
-			// uncomment the below lines to get rid of the 'connection refused:
-			// connect' error:
-
-			// dBuilder.setEntityResolver(new EntityResolver(){
-			// public InputSource resolveEntity(String publicId, String
-			// systemId){
-			// return new InputSource(
-			// new
-			// ByteArrayInputStream("<?xml version='1.0' encoding='UTF-8'?>".getBytes()));
-			// }});
-
 			Document doc = dBuilder.parse(fXmlFile);
-
 			doc.getDocumentElement().normalize();
 
-			System.out.println("Root element :"
-					+ doc.getDocumentElement().getNodeName());
-			NodeList nList = doc.getElementsByTagName("staff");
-			System.out.println("-----------------------");
+			NodeList nList = doc.getElementsByTagName(doc.getDocumentElement().getNodeName());
+			Node baseNode = nList.item(0);
+			NodeList nodes = baseNode.getChildNodes();
 
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-
-				Node nNode = nList.item(temp);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-					Element eElement = (Element) nNode;
-
-					System.out.println("Last Name : "
-							+ getTagValue("lastname", eElement));
-					System.out.println("Nick Name : "
-							+ getTagValue("nickname", eElement));
-					System.out.println("Salary : "
-							+ getTagValue("salary", eElement));
-
+			// parse through each node of the base node (entities)
+			for (int temp = 0; temp < nodes.getLength(); temp++) {
+				Node nNode = nodes.item(temp);
+				NodeList nNodeChildren = nNode.getChildNodes();
+				// parse through each node of each entity
+				for (int i = 0; i < nNodeChildren.getLength(); i++) {
+					Node aNode = nNodeChildren.item(i);
+					if (aNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) aNode;
+						Attr attribName = eElement.getAttributeNode("name");
+						// remove the expression mode node
+						if (attribName.getValue().equals(EXPRESSION_MODE)) {
+							nNode = nNode.removeChild(aNode);
+						}
+					}
 				}
 			}
-//			Element workBenchVersionNode = doc.getElementById("property");
-//			int length = workBenchVersionNode.getAttribute("_workbench");
-//			System.out.println(length);
-			
-			// Do something with the document here.
+			// return the resulting XML tree as a String
+			return printXMLTree(baseNode);
+
 		} catch (ParserConfigurationException e) {
 			Logger.error("ParserConfigurationException:"+e);
+			return null;
 		} catch (SAXException e) {
 			Logger.error("SAXException:"+e);
+			return null;
 		} catch (IOException e) {
 			e.printStackTrace();
 			Logger.error("IOException:"+e);
+			return null;
 		}
 	}
 
-	private String getTagValue(String sTag, Element eElement) {
-		NodeList nlList = eElement.getElementsByTagName(sTag).item(0)
-				.getChildNodes();
-		Node nValue = (Node) nlList.item(0);
-
-		return nValue.getNodeValue();
-	}
-
-	public String readFile(String path, Charset encoding) throws IOException {
-		byte[] encoded = Files.readAllBytes(Paths.get(path));
-		return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+	/**
+	 * Method that returns a DOM node as a String
+	 * @param node
+	 * @return a string
+	 */
+	public static String printXMLTree(Node node) {
+		DOMImplementationLS lsImpl = (DOMImplementationLS)node.getOwnerDocument().getImplementation().getFeature("LS", "3.0");
+		LSSerializer serializer = lsImpl.createLSSerializer();
+		serializer.getDomConfig().setParameter("xml-declaration", true); //by default its true, so set it to false to get String without xml-declaration
+		return serializer.writeToString(node);
 	}
 }
--- org.dawnsci.persistence/src/org/dawnsci/persistence/workflow/xml/TestMomlUpdate.java
+++ org.dawnsci.persistence/src/org/dawnsci/persistence/workflow/xml/TestMomlUpdate.java
@@ -1,14 +1,27 @@
 package org.dawnsci.persistence.workflow.xml;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 public class TestMomlUpdate {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MomlUpdater updater = new MomlUpdater();
-		updater.updateMoml("/scratch/MomlUpdate/before.moml");
-
+		String result = MomlUpdater.updateMoml("/scratch/MomlUpdate/before.moml");
+		// create the new xml file
+		Writer writer = null;
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/scratch/MomlUpdate/result.xml"), "utf-8"));
+			writer.write(result);
+			writer.close();
+		} catch (IOException ex){
+			ex.printStackTrace();
+		}
 	}
 
 }
