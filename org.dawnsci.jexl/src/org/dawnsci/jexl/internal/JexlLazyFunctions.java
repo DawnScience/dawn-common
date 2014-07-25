0098bcd67d30f8eabcf66ef59dff667104193814
matthew.gerring <matthew.gerring@gmail.com>
2014-07-25 11:21:59 +0100

    Added lazy slice which MB does not seem to have added despite giving the old jawjaw about it. Anyone would think he is over-worked :)

--- org.dawnsci.jexl/src/org/dawnsci/jexl/internal/JexlLazyFunctions.java
+++ org.dawnsci.jexl/src/org/dawnsci/jexl/internal/JexlLazyFunctions.java
@@ -1,8 +1,11 @@
 package org.dawnsci.jexl.internal;
 
 import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
+import uk.ac.diamond.scisoft.analysis.dataset.Dataset;
+import uk.ac.diamond.scisoft.analysis.dataset.IDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.ILazyDataset;
 import uk.ac.diamond.scisoft.analysis.dataset.LazyMaths;
+import uk.ac.diamond.scisoft.analysis.dataset.Slice;
 
 /**
  * Functions able to process without loading all the data at once.
@@ -36,8 +39,37 @@ public class JexlLazyFunctions {
 	 * @param axis
 	 * @return
 	 */
-	public static AbstractDataset rsum(final ILazyDataset data, final int axis) {
+	public static IDataset rsum(final ILazyDataset data, final int axis) {
 		return LazyMaths.sum(data, axis);
+	}
+	
+	/**
+	 * Apparently this is not a true rolling sum. However it does the
+	 * job in a lazy way providing the size does not overrun the size
+	 * of a double.
+	 * 
+	 * @param data
+	 * @param axis
+	 * @return
+	 */
+	public static IDataset slice(final ILazyDataset data, final int axis) {
+		return LazyMaths.sum(data, axis);
+	}
+
+	public static IDataset slice(final ILazyDataset data,final int[] start,
+								final int[] stop,
+								final int[] step) {
+		
+		return ((Dataset)data.getSlice(start, stop, step)).squeeze();
+	}
+
+	public static Dataset slice(ILazyDataset data, String sliceString) {
+		
+		Slice[] slices = Slice.convertFromString(sliceString);
+
+		if (slices.length != data.getRank()) throw new IllegalArgumentException("Invalid string");
+
+		return ((Dataset)data.getSlice(slices)).squeeze();
 	}
 
 }
