b7bdad8e9752f2a4e1179afd363a62c735e3e4a5
matthew.gerring <matthew.gerring@gmail.com>
2014-07-28 16:41:03 +0100

    Made lazy in creating the service.

--- org.dawnsci.conversion/src/org/dawnsci/conversion/ConversionServiceImpl.java
+++ org.dawnsci.conversion/src/org/dawnsci/conversion/ConversionServiceImpl.java
@@ -14,8 +14,6 @@ import org.dawnsci.conversion.converters.ImageConverter;
 import org.dawnsci.conversion.converters.ImagesToHDFConverter;
 import org.dawnsci.conversion.converters.VisitorConversion;
 
-import uk.ac.diamond.scisoft.analysis.dataset.ILazyDataset;
-
 public class ConversionServiceImpl implements IConversionService {
 	
 	static {
