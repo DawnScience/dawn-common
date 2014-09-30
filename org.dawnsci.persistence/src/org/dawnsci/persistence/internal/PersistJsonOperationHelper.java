71e8375fcb2b768bab3c92ff288023ecc44d8f56
Jacob Filik <jacob.filik@diamond.ac.uk>
2014-09-30 15:32:27 +0100

    Add save and pass through to operation persistance

--- org.dawnsci.persistence/src/org/dawnsci/persistence/internal/PersistJsonOperationHelper.java
+++ org.dawnsci.persistence/src/org/dawnsci/persistence/internal/PersistJsonOperationHelper.java
@@ -8,19 +8,24 @@
  */
 package org.dawnsci.persistence.internal;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 import java.util.List;
 
-import ncsa.hdf.object.h5.H5ScalarDS;
-
 import org.dawb.common.services.ServiceManager;
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.api.processing.IOperation;
 import org.eclipse.dawnsci.analysis.api.processing.IOperationService;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.model.IOperationModel;
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
 import org.eclipse.dawnsci.hdf5.IHierarchicalDataFile;
+import org.eclipse.dawnsci.hdf5.Nexus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import uk.ac.diamond.scisoft.analysis.io.LoaderFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -29,6 +34,13 @@ public class PersistJsonOperationHelper {
 
 	ObjectMapper mapper;
 	IOperationService service;
+	
+	private final static String DATA = "data";
+	private final static String ID = "id";
+	private final static String SLASH = "/";
+	private final static String NAME = "name";
+	private final static String PASS = "passed";
+	private final static String SAVE = "saved";
 	
 	private final static Logger logger = LoggerFactory.getLogger(PersistJsonOperationHelper.class);
 	
@@ -40,24 +52,48 @@ public class PersistJsonOperationHelper {
 		List<String> memberList = file.memberList(group);
 
 		int i = 0;
-		String[] name = new String[]{PersistenceConstants.PROCESS_ENTRY+ "/"+ Integer.toString(i)};
+		String[] name = new String[]{PersistenceConstants.PROCESS_ENTRY+SLASH+ Integer.toString(i)};
 
 		while (memberList.contains(name[0])){
-			Object data = file.getData(name[0] +"/data");
-			Object id = file.getData(name[0] +"/id");
-			String json = ((String[])((H5ScalarDS)data).getData())[0];
-			String sid = ((String[])((H5ScalarDS)id).getData())[0];
-
+			IDataset data = LoaderFactory.getDataSet(file.getPath(), name[0] +SLASH + DATA, null);
+			IDataset id = LoaderFactory.getDataSet(file.getPath(), name[0] +SLASH + ID, null);
+			String json = data.getObject(0).toString();
+			String sid = id.getObject(0).toString();
+			
+			boolean p = false;
+			boolean s = false;
+			
+			try {
+				IDataset pass = LoaderFactory.getDataSet(file.getPath(), name[0] +SLASH + PASS, null);
+				IDataset save = LoaderFactory.getDataSet(file.getPath(), name[0] +SLASH + SAVE, null);
+				p = pass.getInt(0) == 0 ? false : true;
+				s = save.getInt(0) == 0 ? false : true;
+			} catch (Exception e) {
+				logger.error("Could not read pass/save nodes", e);
+			}
+			
 			if (service == null) service = (IOperationService)ServiceManager.getService(IOperationService.class);
 
 			IOperation op = service.create(sid);
 			Class<? extends IOperationModel> modelClass = service.getModelClass(sid);
 			if (mapper == null) mapper = new ObjectMapper();
+			
+			try {
 			IOperationModel readValue = mapper.readValue(json, modelClass);
 			op.setModel(readValue);
+			} catch (Exception e) {
+				logger.error("Could not read model values", e);
+				Class modelType = (Class)((ParameterizedType)op.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+				IOperationModel model  = (IOperationModel) modelType.newInstance();
+				op.setModel(model);
+			}
+			
+			op.setPassUnmodifiedData(p);
+			op.setStoreOutput(s);
+			
 			opList.add(op);
 
-			name[0] = "/entry/process/"+ Integer.toString(++i);
+			name[0] = PersistenceConstants.PROCESS_ENTRY +"/"+ Integer.toString(++i);
 
 		}
 
@@ -67,7 +103,7 @@ public class PersistJsonOperationHelper {
 	public void writeOperations(IHierarchicalDataFile file, IOperation<? extends IOperationModel, ? extends OperationData>[] operations) throws Exception {
 			
 		String process = file.group(PersistenceConstants.PROCESS_ENTRY);
-		file.setNexusAttribute(process, "NXprocess");
+		file.setNexusAttribute(process, Nexus.PROCESS);
 
 		for (int i = 0; i < operations.length; i++) {
 			writeOperationToProcess(file, process, i, operations[i]);
@@ -80,12 +116,19 @@ public class PersistJsonOperationHelper {
 		String opId = op.getId();
 		String name = op.getName();
 		String modelJson = getModelJson(op.getModel());
+		IDataset boolTrue = DatasetFactory.ones(new int[] {1}, Dataset.INT);
+		IDataset boolFalse =  DatasetFactory.zeros(new int[] {1}, Dataset.INT);
+		
+		IDataset pass = op.isPassUnmodifiedData() ? boolTrue : boolFalse;
+		IDataset save = op.isStoreOutput() ? boolTrue : boolFalse;
 		
 		String note = file.group(Integer.toString(i), group);
-		file.setNexusAttribute(note, "NXnote");
-		file.createStringDataset("name", name, note);
-		file.createStringDataset("id", opId, note);
-		file.createStringDataset("data", modelJson, note);
+		file.setNexusAttribute(note, Nexus.NOTE);
+		file.createStringDataset(NAME, name, note);
+		file.createStringDataset(ID, opId, note);
+		file.createStringDataset(DATA, modelJson, note);
+		file.createDataset(SAVE, save, note);
+		file.createDataset(PASS, pass, note);
 		
 	}
 	
