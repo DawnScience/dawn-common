203813e870893469cc4020cc10d8035e158d03a8
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-10-13 13:53:15 +0100

    Add new transform boofcv implementation of IImageTransform

---
+++ org.dawnsci.boofcv/src/org/dawnsci/boofcv/internal/BoofCVImageTransformImpl.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2012 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.dawnsci.boofcv.internal;
+
+import org.dawnsci.boofcv.converter.ConvertIDataset;
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.image.IImageTransform;
+
+import boofcv.alg.distort.DistortImageOps;
+import boofcv.alg.interpolate.TypeInterpolate;
+import boofcv.struct.image.ImageFloat32;
+
+/**
+ * Implementation of IImageTransform<br>
+ * 
+ * This class is internal and not supposed to be used out of this bundle.
+ * 
+ * @author wqk87977
+ *
+ */
+public class BoofCVImageTransformImpl implements IImageTransform {
+
+	static {
+		System.out.println("Starting BoofCV image transform service.");
+	}
+
+	public BoofCVImageTransformImpl() {
+		// Important do nothing here, OSGI may start the service more than once.
+	}
+
+	@Override
+	public IDataset rotate(IDataset data, double angle) throws Exception {
+		if (data.getShape().length != 2)
+			throw new Exception("Data shape is not 2D");
+		ImageFloat32 image = ConvertIDataset.convertFrom(data, ImageFloat32.class, 1);
+		ImageFloat32 rotated = new ImageFloat32(image.height, image.width);
+
+		DistortImageOps.rotate(image, rotated, TypeInterpolate.BILINEAR, (float)Math.toRadians(angle));
+		return ConvertIDataset.convertTo(rotated, true);
+	}
+
+}
