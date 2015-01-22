504835964bc0f84783cfdef08efe07b06cd2dab7
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2015-01-22 16:50:06 +0000

    Used another class to inject conversion service and use in ConvertWizard

---
+++ org.dawnsci.conversion.ui/src/org/dawnsci/conversion/ui/ConversionServiceLoader.java
@@ -0,0 +1,33 @@
+package org.dawnsci.conversion.ui;
+
+import org.dawb.common.services.conversion.IConversionService;
+
+/**
+ * This class is used to inject the conversion service using OSGI and retrieve in ConvertWizard
+ * @author wqk87977
+ *
+ */
+public class ConversionServiceLoader {
+
+	private static IConversionService service;
+
+	/**
+	 * Used for OSGI injection
+	 */
+	public ConversionServiceLoader() {
+		
+	}
+
+	/**
+	 * Injected by OSGI
+	 * @param cs
+	 */
+	public static void setService(IConversionService cs) {
+		service = cs;
+	}
+
+	public static IConversionService getService() {
+		return service;
+	}
+
+}
