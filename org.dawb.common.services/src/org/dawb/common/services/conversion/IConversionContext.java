a67f315e7fc82c2aba1fe88aee862a0c485aa128
Jun Aishima <jun.aishima@diamond.ac.uk>
2015-02-03 10:56:12 +0000

    DAWNSCI-5799 add rank 1 for exporting NCD data
     * due to squeeze, rank of 1 must be included to allow reduced and
       averaged data to be exported

--- org.dawb.common.services/src/org/dawb/common/services/conversion/IConversionContext.java
+++ org.dawb.common.services/src/org/dawb/common/services/conversion/IConversionContext.java
@@ -42,7 +42,7 @@ public interface IConversionContext {
 		ALIGNED_FROM_3D(" align stack of images", true, false, 2, 3, 4, 5),
 		H5_FROM_IMAGEDIR(" nexus stack from directory of images", true, false, 2),
 		H5_FROM_1D(" nexus from 1D data",   true, false,  1),
-		CUSTOM_NCD(" ascii from NCD data",     true, 2,3,4,5,6),
+		CUSTOM_NCD(" ascii from NCD data",     true, 1,2,3,4,5,6),
 		CUSTOM_TOMO(" tiff from tomography nexus file(s) [nxtomo]",    true, 3),
 		COMPARE(" compare data",    true,  false, 0,1,2,3,4,5),
 		PROCESS(" process data", false, true, 1,2,3,4,5);
