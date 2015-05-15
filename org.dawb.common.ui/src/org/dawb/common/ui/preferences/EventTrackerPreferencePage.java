de81ea805363ecd5d0cf0174a0dc848714c38fef
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-05-15 13:44:13 +0100

    Add a StringFieldEditor to the preferencepage

--- org.dawb.common.ui/src/org/dawb/common/ui/preferences/EventTrackerPreferencePage.java
+++ org.dawb.common.ui/src/org/dawb/common/ui/preferences/EventTrackerPreferencePage.java
@@ -5,12 +5,15 @@ import org.eclipse.dawnsci.plotting.api.preferences.BasePlottingConstants;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 public class EventTrackerPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	private BooleanFieldEditor isTrackerEnabled;
+	private StringFieldEditor googleTrackCode;
 
 	@Override
 	public void init(IWorkbench workbench) {
@@ -24,5 +27,11 @@ public class EventTrackerPreferencePage extends FieldEditorPreferencePage implem
 	protected void createFieldEditors() {
 		isTrackerEnabled = new BooleanFieldEditor(BasePlottingConstants.IS_TRACKER_ENABLED, "Enable usage tracking", getFieldEditorParent());
 		addField(isTrackerEnabled);
+
+		googleTrackCode = new StringFieldEditor(BasePlottingConstants.ANALYTICS_TRACK_CODE, "Tracking code:", getFieldEditorParent());
+		addField(googleTrackCode);
+//		Text trackText = googleTrackCode.getTextControl(getFieldEditorParent());
+//		trackText.setToolTipText("Google Analytics tracking code");
 	}
+
 }
