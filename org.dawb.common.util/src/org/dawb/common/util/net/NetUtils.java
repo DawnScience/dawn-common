6c67e2d351cba386a97bcbe12953167c3ca0ad61
Simon Berriman <simon.berriman@diamond.ac.uk>
2015-07-01 11:07:54 +0100

    Add method to find free port at random

--- org.dawb.common.util/src/org/dawb/common/util/net/NetUtils.java
+++ org.dawb.common.util/src/org/dawb/common/util/net/NetUtils.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- */ 
+ */
 package org.dawb.common.util.net;
 
 import java.io.IOException;
@@ -14,21 +14,47 @@ import java.net.ServerSocket;
 
 public class NetUtils {
 
-	/**
-	 * Attempts to get a free port starting at the passed in port and
-	 * working up.
-	 * 
-	 * @param startPort
-	 * @return
-	 */
+	    /**
+     * Attempts to get a free port starting at the passed in port and working up.
+     *
+     * @param startPort
+     * @return
+     */
 	public static int getFreePort(final int startPort) {
-		
+
 	    int port = startPort;
 	    while(!NetUtils.isPortFree(port)) port++;
-	    	
+
 	    return port;
 	}
 
+    /**
+     * Attempts to get a free port by asking a ServerSocket to find one, and then closing that socket and returning the port it used.
+     *
+     * @return
+     */
+    public static int getFreePort() throws IOException {
+        int port = 0;
+        ServerSocket s = null;
+        try {
+            s = new ServerSocket(0);
+            port = s.getLocalPort();
+
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+
+        if (port < 0) {
+            // The idea of this check is based on some code in PyDev which
+            // implies that ServerSocket
+            // can return -1 when a firewall configuration is causing a problem
+            throw new IOException("Unable to obtain free port, is a firewall running?");
+        }
+
+        return port;
+    }
 
 	/**
 	 * Checks if a port is free.
