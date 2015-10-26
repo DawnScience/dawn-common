9ecae9dafe2a974fe141f21d8f5d472b2da54db0
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-10-26 17:00:22 +0000

    Add equal tests on roibean objects

--- org.dawnsci.persistence.test/src/org/dawnsci/persistence/test/ROIBeanConverterTest.java
+++ org.dawnsci.persistence.test/src/org/dawnsci/persistence/test/ROIBeanConverterTest.java
@@ -15,10 +15,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.dawnsci.analysis.dataset.roi.CircularROI;
+import org.eclipse.dawnsci.analysis.dataset.roi.LinearROI;
+import org.eclipse.dawnsci.analysis.dataset.roi.PointROI;
 import org.eclipse.dawnsci.analysis.dataset.roi.PolylineROI;
 import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROI;
 import org.eclipse.dawnsci.analysis.dataset.roi.SectorROI;
 import org.eclipse.dawnsci.analysis.dataset.roi.json.CircularROIBean;
+import org.eclipse.dawnsci.analysis.dataset.roi.json.LinearROIBean;
+import org.eclipse.dawnsci.analysis.dataset.roi.json.PointROIBean;
 import org.eclipse.dawnsci.analysis.dataset.roi.json.PolylineROIBean;
 import org.eclipse.dawnsci.analysis.dataset.roi.json.ROIBean;
 import org.eclipse.dawnsci.analysis.dataset.roi.json.ROIBeanFactory;
@@ -31,11 +35,14 @@ public class ROIBeanConverterTest {
 
 	double[] startPoint = new double[2];
 	double[] lengths = new double[2];
+	double[] endPoint = new double[2];
 
 	@Before
 	public void setup() {
 		startPoint[0] = 100;
 		startPoint[1] = 100;
+		endPoint[0] = 500;
+		endPoint[1] = 300;
 		lengths[0] = 400;
 		lengths[1] = 200;
 	}
@@ -105,5 +112,72 @@ public class ROIBeanConverterTest {
 		assertArrayEquals(angles, sroi.getAngles(), 0);
 		assertEquals(dpp, sroi.getDpp(), 0);
 		assertEquals(symmetry, sroi.getSymmetry());
+	}
+
+	@Test
+	public void testPointROIBeanEqual() throws Exception {
+		ROIBean rbean = new PointROIBean();
+		((PointROIBean)rbean).setName("point");
+		((PointROIBean)rbean).setStartPoint(startPoint);
+		((PointROIBean)rbean).setType("PointROI");
+		PointROI proi = (PointROI)ROIBeanFactory.decapsulate(rbean);
+		PointROIBean resultBean = (PointROIBean)ROIBeanFactory.encapsulate(proi);
+		assertEquals(rbean, resultBean);
+	}
+
+	@Test
+	public void testLinearROIBeanEqual() throws Exception {
+		ROIBean rbean = new LinearROIBean();
+		((LinearROIBean)rbean).setName("line");
+		((LinearROIBean)rbean).setStartPoint(startPoint);
+		((LinearROIBean)rbean).setType("LinearROI");
+		((LinearROIBean)rbean).setEndPoint(endPoint);
+		LinearROI lroi = (LinearROI)ROIBeanFactory.decapsulate(rbean);
+		LinearROIBean resultBean = (LinearROIBean)ROIBeanFactory.encapsulate(lroi);
+		assertEquals(rbean, resultBean);
+	}
+
+	@Test
+	public void testRectangularROIBeanEqual() throws Exception {
+		ROIBean rbean = new RectangularROIBean();
+		((RectangularROIBean)rbean).setName("rectangle");
+		((RectangularROIBean)rbean).setStartPoint(startPoint);
+		((RectangularROIBean)rbean).setType("RectangularROI");
+		((RectangularROIBean)rbean).setLengths(lengths);
+		((RectangularROIBean)rbean).setEndPoint(endPoint);
+		((RectangularROIBean)rbean).setAngle(0);
+		RectangularROI rroi = (RectangularROI)ROIBeanFactory.decapsulate(rbean);
+		RectangularROIBean resultBean = (RectangularROIBean)ROIBeanFactory.encapsulate(rroi);
+		assertEquals(rbean, resultBean);
+	}
+
+	@Test
+	public void testCircularROIBeanEqual()throws Exception {
+		//CircularROI
+		double radius = 100;
+		ROIBean rbean = new CircularROIBean();
+		((CircularROIBean)rbean).setName("circle");
+		((CircularROIBean)rbean).setStartPoint(startPoint);
+		((CircularROIBean)rbean).setType("CircularROI");
+		((CircularROIBean)rbean).setRadius(radius);
+		CircularROI croi = (CircularROI)ROIBeanFactory.decapsulate(rbean);
+		CircularROIBean resultBean = (CircularROIBean)ROIBeanFactory.encapsulate(croi);
+		assertEquals(rbean, resultBean);
+	}
+
+	@Test
+	public void testSectorROIBeanEqual() throws Exception {
+		//SectorROI
+		double[] radii = {30, 50}, angles = {6, 9};
+		double dpp = 20; int symmetry = 5;
+		ROIBean rbean = new SectorROIBean();
+		((SectorROIBean)rbean).setAngles(angles);
+		((SectorROIBean)rbean).setStartPoint(startPoint);
+		((SectorROIBean)rbean).setRadii(radii);
+		((SectorROIBean)rbean).setDpp(dpp);
+		((SectorROIBean)rbean).setSymmetry(symmetry);
+		SectorROI sroi = (SectorROI)ROIBeanFactory.decapsulate(rbean);
+		SectorROIBean resultBean = (SectorROIBean)ROIBeanFactory.encapsulate(sroi);
+		assertEquals(rbean, resultBean);
 	}
 }
