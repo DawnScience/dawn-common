a2d1bbb222cf2d569562d8844d2f061e64e1125f
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-06-10 18:08:15 +0100

    Add new JSON marshaller OSGi service

--- org.dawnsci.persistence/src/org/dawnsci/persistence/ServiceLoader.java
+++ org.dawnsci.persistence/src/org/dawnsci/persistence/ServiceLoader.java
@@ -1,11 +1,17 @@
 package org.dawnsci.persistence;
 
 import org.eclipse.dawnsci.analysis.api.io.ILoaderService;
+import org.eclipse.dawnsci.analysis.api.persistence.IMarshallerService;
 
 public class ServiceLoader {
 
 	private static ILoaderService sloader;
 
+	private static IMarshallerService mservice;
+
+	/**
+	 * Do nothing, loaded by OSGi
+	 */
 	public ServiceLoader() {
 		
 	}
@@ -16,5 +22,13 @@ public class ServiceLoader {
 
 	public static ILoaderService getLoaderService() {
 		return sloader;
+	}
+
+	public static void setJSONMarshallerService(IMarshallerService js) {
+		mservice = js;
+	}
+
+	public static IMarshallerService getJSONMarshallerService() {
+		return mservice;
 	}
 }
