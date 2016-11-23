d95e9f56c2915c8c655f5001b325527bdac2e09b
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-11-23 15:41:19 +0000

    Use Homography and better parameters for Hessian registration

--- org.dawnsci.boofcv/src/org/dawnsci/boofcv/registration/ImageHessianRegistration.java
+++ org.dawnsci.boofcv/src/org/dawnsci/boofcv/registration/ImageHessianRegistration.java
@@ -8,10 +8,6 @@
  */
 package org.dawnsci.boofcv.registration;
 
-import georegression.fitting.affine.ModelManagerAffine2D_F64;
-import georegression.struct.affine.Affine2D_F64;
-import georegression.struct.point.Point2D_F64;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,11 +24,13 @@ import boofcv.abst.feature.detdesc.DetectDescribePoint;
 import boofcv.abst.feature.detect.interest.ConfigFastHessian;
 import boofcv.alg.distort.ImageDistort;
 import boofcv.alg.distort.PixelTransformAffine_F32;
+import boofcv.alg.distort.PixelTransformHomography_F32;
 import boofcv.alg.distort.impl.DistortSupport;
+import boofcv.alg.distort.impl.ImplImageDistort_F32;
 import boofcv.alg.feature.UtilFeature;
 import boofcv.alg.interpolate.impl.ImplBilinearPixel_F32;
-import boofcv.alg.sfm.robust.DistanceAffine2D;
-import boofcv.alg.sfm.robust.GenerateAffine2D;
+import boofcv.alg.sfm.robust.DistanceHomographySq;
+import boofcv.alg.sfm.robust.GenerateHomographyLinear;
 import boofcv.factory.feature.associate.FactoryAssociation;
 import boofcv.factory.feature.detdesc.FactoryDetectDescribe;
 import boofcv.struct.feature.AssociatedIndex;
@@ -42,6 +40,10 @@ import boofcv.struct.geo.AssociatedPair;
 import boofcv.struct.image.ImageFloat32;
 import boofcv.struct.image.ImageSingleBand;
 import boofcv.struct.image.MultiSpectral;
+import georegression.fitting.homography.ModelManagerHomography2D_F64;
+import georegression.struct.affine.Affine2D_F64;
+import georegression.struct.homography.Homography2D_F64;
+import georegression.struct.point.Point2D_F64;
 
 /**
  * This code is heavily based on the example code given as part of the BoofCV
@@ -58,6 +60,41 @@ public class ImageHessianRegistration {
 			T imageA, T imageB, DetectDescribePoint<T, FD> detDesc,
 			AssociateDescription<FD> associate,
 			ModelMatcher<Affine2D_F64, AssociatedPair> modelMatcher) {
+		// get the length of the description
+		List<Point2D_F64> pointsA = new ArrayList<Point2D_F64>();
+		FastQueue<FD> descA = UtilFeature.createQueue(detDesc, 100);
+		List<Point2D_F64> pointsB = new ArrayList<Point2D_F64>();
+		FastQueue<FD> descB = UtilFeature.createQueue(detDesc, 100);
+		// extract feature locations and descriptions from each image
+		describeImage(imageA, detDesc, pointsA, descA);
+		describeImage(imageB, detDesc, pointsB, descB);
+		// Associate features between the two images
+		associate.setSource(descA);
+		associate.setDestination(descB);
+		associate.associate();
+		// create a list of AssociatedPairs that tell the model matcher how a
+		// feature moved
+		FastQueue<AssociatedIndex> matches = associate.getMatches();
+		List<AssociatedPair> pairs = new ArrayList<>();
+		for (int i = 0; i < matches.size(); i++) {
+			AssociatedIndex match = matches.get(i);
+			Point2D_F64 a = pointsA.get(match.src);
+			Point2D_F64 b = pointsB.get(match.dst);
+			pairs.add(new AssociatedPair(a, b, false));
+		}
+		// find the best fit model to describe the change between these images
+		if (!modelMatcher.process(pairs)) {
+			logger.info("Model matcher failed: images cannot be registered");
+			return null;
+		}
+		// return the found image transform
+		return modelMatcher.getModelParameters().copy();
+	}
+
+	private static <T extends ImageSingleBand<?>, FD extends TupleDesc<?>> Homography2D_F64 computeTransform2(
+			T imageA, T imageB, DetectDescribePoint<T, FD> detDesc,
+			AssociateDescription<FD> associate,
+			ModelMatcher<Homography2D_F64, AssociatedPair> modelMatcher) {
 		// get the length of the description
 		List<Point2D_F64> pointsA = new ArrayList<Point2D_F64>();
 		FastQueue<FD> descA = UtilFeature.createQueue(detDesc, 100);
@@ -125,20 +162,30 @@ public class ImageHessianRegistration {
 				.greedy(scorer, 2, true);
 		// fit the images using a homography. This works well for rotations and
 		// distant objects.
-		ModelManager<Affine2D_F64> manager = new ModelManagerAffine2D_F64();
+//		ModelManager<Affine2D_F64> manager = new ModelManagerAffine2D_F64();
+		ModelManager<Homography2D_F64> manager = new ModelManagerHomography2D_F64();
+		
+//		GenerateAffine2D modelFitter = new GenerateAffine2D();
+		GenerateHomographyLinear modelFitter = new GenerateHomographyLinear(true);
+		
+//		DistanceAffine2D distance = new DistanceAffine2D();
+		DistanceHomographySq distance = new DistanceHomographySq();
 
-		GenerateAffine2D modelFitter = new GenerateAffine2D();
-
-		DistanceAffine2D distance = new DistanceAffine2D();
-
-		ModelMatcher<Affine2D_F64, AssociatedPair> modelMatcher = new Ransac<Affine2D_F64, AssociatedPair>(
-				123, manager, modelFitter, distance, 60, 9);
-		Affine2D_F64 H = computeTransform(imageA, imageB, detDesc,
-				associate, modelMatcher);
+//		ModelMatcher<Affine2D_F64, AssociatedPair> modelMatcher = new Ransac<Affine2D_F64, AssociatedPair>(
+//				10, manager, modelFitter, distance, 100000, 1);
+		// TODO Add UI to manage different algorithms and also different parameters
+		ModelMatcher<Homography2D_F64, AssociatedPair> modelMatcher = new Ransac<Homography2D_F64, AssociatedPair>(
+				10, manager, modelFitter, distance, 10000, 1);
+		
+//		Affine2D_F64 H = computeTransform(imageA, imageB, detDesc, associate, modelMatcher);
+		Homography2D_F64 H = computeTransform2(imageA, imageB, detDesc, associate, modelMatcher);
+		
 		if (H == null) {
 			return (ImageFloat32) imageB;
 		}
+//		ImageSingleBand<?> output = renderHessianRegistration(imageA, (ImageFloat32) imageB, H);
 		ImageSingleBand<?> output = renderHessianRegistration(imageA, (ImageFloat32) imageB, H);
+
 		return output;
 	}
 
@@ -160,7 +207,7 @@ public class ImageHessianRegistration {
 		PixelTransformAffine_F32 model = new PixelTransformAffine_F32();
 		ImageDistort<MultiSpectral<ImageFloat32>, MultiSpectral<ImageFloat32>> distort = DistortSupport
 				.createDistortMS(ImageFloat32.class, model,
-						new ImplBilinearPixel_F32(), true, null);
+						new ImplBilinearPixel_F32((ImageFloat32) imageA), true, null); //ImplBilinearPixel_F32(),
 		
 		// Apply transformation to register the image
 		Affine2D_F64 fromWorkToB = fromWorkToA.concat(fromAtoB, null);
@@ -175,6 +222,42 @@ public class ImageHessianRegistration {
 		workrgb.bands[0] = (ImageFloat32)work;
 		
 		distort.apply(colorrgb, workrgb);
+		// create rgb version of work - to convert to BufferedImage
+		return workrgb.bands[0];
+	}
+
+	private static ImageSingleBand<?> renderHessianRegistration(
+			ImageSingleBand<?> imageA, ImageSingleBand<?> colorB,
+			Homography2D_F64 fromAtoB) {
+		// specify size of output image
+		int outputWidth = imageA.getWidth();
+		int outputHeight = imageA.getHeight();
+		// Where the output images are rendered into
+		ImageFloat32 work = new ImageFloat32(outputWidth, outputHeight);
+		Homography2D_F64 fromAToWork = new Homography2D_F64();
+		Homography2D_F64 fromWorkToA = fromAToWork.invert(null);
+
+		// Used to render the results onto an image
+		PixelTransformHomography_F32 model = new PixelTransformHomography_F32();
+		ImplImageDistort_F32<ImageFloat32> distort = new ImplImageDistort_F32<ImageFloat32>(
+				new ImplBilinearPixel_F32(), null);
+		distort.setModel(model);
+
+		// Apply transformation to register the image
+		Homography2D_F64 fromWorkToB = fromWorkToA.concat(fromAtoB, null);
+
+		model.set(fromWorkToB);
+		
+		MultiSpectral<ImageFloat32> colorrgb = new MultiSpectral<>(
+				ImageFloat32.class, outputWidth, outputHeight, 1);
+		colorrgb.bands[0] = (ImageFloat32)colorB;
+		MultiSpectral<ImageFloat32> workrgb = new MultiSpectral<>(
+				ImageFloat32.class, outputWidth, outputHeight, 1);
+		workrgb.bands[0] = (ImageFloat32)work;
+		
+//		distort.apply(colorrgb, workrgb);
+		distort.apply((ImageFloat32)colorrgb.bands[0], (ImageFloat32) workrgb.bands[0], 0, 0, outputWidth, outputHeight);
+		
 		// create rgb version of work - to convert to BufferedImage
 		return workrgb.bands[0];
 	}
