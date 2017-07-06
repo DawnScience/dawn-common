29ea7c3e1e8d836aca7f4cc3e5e7c61068f189c6
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-07-06 11:39:31 +0100

    SCI-5914: remove B18 conversion pages
    Will be added to scisoft-spectroscopy

--- org.dawnsci.conversion/src/org/dawnsci/conversion/schemes/B18AverageConverterScheme.java
+++
@@ -1,9 +0,0 @@
-package org.dawnsci.conversion.schemes;
-
-import org.dawnsci.conversion.converters.B18AverageConverter;
-
-public class B18AverageConverterScheme extends AbstractConversionScheme {
-	public B18AverageConverterScheme() {
-		super(B18AverageConverter.class, " average B18 data", true, false, false, 1);
-	}
-}
--- org.dawnsci.conversion/src/org/dawnsci/conversion/schemes/B18ReprocessAsciiConverterScheme.java
+++
@@ -1,9 +0,0 @@
-package org.dawnsci.conversion.schemes;
-
-import org.dawnsci.conversion.converters.B18ReprocessAsciiConverter;
-
-public class B18ReprocessAsciiConverterScheme extends AbstractConversionScheme {
-	public B18ReprocessAsciiConverterScheme() {
-		super(B18ReprocessAsciiConverter.class, " reprocess B18 ascii", true, false, false, 1);
-	}
-}
