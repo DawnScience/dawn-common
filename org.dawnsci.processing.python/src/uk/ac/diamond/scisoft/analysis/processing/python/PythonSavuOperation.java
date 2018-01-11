a96d37b0ebe02d5fee3eb957521a43685b550449
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-01-11 09:33:07 +0000

    SCI-6372: merge AbstractPythonSavuOperation and PythonSavuOperation

--- org.dawnsci.processing.python/src/uk/ac/diamond/scisoft/analysis/processing/python/PythonSavuOperation.java
+++ org.dawnsci.processing.python/src/uk/ac/diamond/scisoft/analysis/processing/python/PythonSavuOperation.java
@@ -2,15 +2,26 @@ package uk.ac.diamond.scisoft.analysis.processing.python;
 
 import java.util.Map;
 
+import org.dawnsci.python.rpc.AnalysisRpcPythonPyDevService;
+import org.dawnsci.python.rpc.PythonRunSavuService;
 import org.eclipse.dawnsci.analysis.api.processing.Atomic;
 import org.eclipse.dawnsci.analysis.api.processing.OperationData;
 import org.eclipse.dawnsci.analysis.api.processing.OperationException;
 import org.eclipse.dawnsci.analysis.api.processing.OperationRank;
+import org.eclipse.dawnsci.analysis.dataset.operations.AbstractOperation;
+import org.eclipse.january.IMonitor;
 import org.eclipse.january.MetadataException;
 import org.eclipse.january.dataset.IDataset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Atomic
-public class PythonSavuOperation extends AbstractPythonSavuOperation<PythonSavuModel> {
+public class PythonSavuOperation extends AbstractOperation<PythonSavuModel,OperationData> {
+
+	AnalysisRpcPythonPyDevService s = null;
+	PythonRunSavuService pythonRunSavuService;
+	private static final Logger logger = LoggerFactory.getLogger(PythonSavuOperation.class);
+	protected Integer outputRank = null;
 
 	@Override
 	public String getId() {
@@ -35,8 +46,7 @@ public class PythonSavuOperation extends AbstractPythonSavuOperation<PythonSavuM
 		return OperationRank.SAME;
 	}
 
-	@Override
-	protected Map<String, Object> packInput(IDataset input) {
+	private Map<String, Object> packInput(IDataset input) {
 		if (model.getPluginRank() == 2) {
 			return OperationToPythonUtils.packImage(input);
 		} else if (model.getPluginRank() == 1) {
@@ -47,8 +57,7 @@ public class PythonSavuOperation extends AbstractPythonSavuOperation<PythonSavuM
 	}
 
 
-	@Override
-	protected OperationData packAndValidateMap(Map<String, Object> output) {
+	private OperationData packAndValidateMap(Map<String, Object> output) {
 		try {
 			if (model.getPluginRank() == 2) {
 				return OperationToPythonUtils.unpackImage(output);
@@ -62,4 +71,48 @@ public class PythonSavuOperation extends AbstractPythonSavuOperation<PythonSavuM
 		}
 	}
 
+	@Override
+	public void init() {
+		long t = System.currentTimeMillis();
+		try {
+			s = AnalysisRpcPythonPyDevService.create();
+			pythonRunSavuService = new PythonRunSavuService(s);
+			logger.debug("Time to start service: " + (System.currentTimeMillis()-t));
+		} catch (Exception e) {
+			logger.debug("Failed to start service in: " + (System.currentTimeMillis()-t));
+			logger.error("Could not create script service!");
+			throw new OperationException(this, "Could not create script service!");			
+		}
+		
+	}
+	
+	@Override
+	public void dispose() {
+		if (s != null) s.stop();
+	}
+	
+	protected OperationData process(IDataset input, IMonitor monitor) throws OperationException {
+		
+		if (s == null || pythonRunSavuService == null) throw new OperationException(this, "Could not create python interpreter");
+		logger.debug("pluginname and path are"+model.getPluginName()+" "+model.getPluginPath());
+		if (model.getPluginName() == null || model.getPluginPath()== null) throw new OperationException(this, "Path to script not set");
+		
+		Map<String,Object> inputs = packInput(input);
+		
+		try {
+			// need to pass a dictionary of inputs into here.
+//			generate a hashmap in model, with a single getter to pass it here
+			String pluginPath = model.getPluginPath();
+			Map <String, Object> parameters = model.getParameters();
+			Boolean metaDataOnly = model.isMetaDataOnly();
+			logger.debug(parameters.toString());
+			if (outputRank == null) {
+				outputRank = pythonRunSavuService.get_output_rank(pluginPath, inputs, parameters);
+			}
+			Map<String, Object> out = pythonRunSavuService.runSavu(pluginPath,parameters,metaDataOnly,inputs);
+			return packAndValidateMap(out);
+		} catch (Exception e) {
+			throw new OperationException(this, "Could not run " + model.getPluginName() + " due to " + e.getMessage());
+		}
+	}
 }
