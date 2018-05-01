730feb0eef2dc1cee5b777eba71b749a6334481a
Peter Chang <peter.chang@diamond.ac.uk>
2018-05-01 10:47:04 +0100

    # WARNING: head commit changed in the meantime
    Fix get version to work when running in the IDE

--- org.dawb.common.util/src/org/dawb/common/util/eclipse/BundleUtils.java
+++ org.dawb.common.util/src/org/dawb/common/util/eclipse/BundleUtils.java
@@ -12,6 +12,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -83,34 +89,71 @@ public class BundleUtils {
 		} else {
 			path = hDirectory.getAbsolutePath();
 		}
-        return path;
+		return path;
 	}
-	
-	private static Pattern FEATURE_MATCH = Pattern.compile("org.dawnsci.base.product.feature_(.+)");
+
+	private final static String FEATURE_PLUGIN = "org.dawnsci.base.product.feature";
+
+	private static Pattern FEATURE_MATCH = Pattern.compile(FEATURE_PLUGIN + "_(.+)");
+
 	/**
-	 * Looks at installed features, gets newest uk.ac.diamond.dawn.product.feature
+	 * Looks at installed features, gets newest org.dawnsci.base.product.feature
 	 * and returns that version.
 	 * 
-	 * @return null if cannot find a dawn feature (might happen in debug mode)
+	 * @return null if cannot find a dawn feature
 	 */
 	public static String getDawnVersion() {
 		
-		final File   dir = new File(getEclipseHome(), "features");
-		if (!dir.exists()) return null;
-		final File[] fa  = dir.listFiles();
-		
+		File dir = new File(getEclipseHome(), "features");
+		if (!dir.exists()) {
+			return null;
+		}
+
 		long date = -1;
 		String version = null;
-		for (File sd : fa) {
+		for (File sd : dir.listFiles()) {
 			if (!sd.isDirectory()) continue;
 			Matcher matcher = FEATURE_MATCH.matcher(sd.getName());
 			if (matcher.matches()) {
-				if (date<sd.lastModified()) {
+				if (date < sd.lastModified()) {
 					date    = sd.lastModified();
 					version = matcher.group(1); 
 				}
 			}
 		}
+
+		if (version == null) { // running in Eclipse so dir=workspace/tp/features
+			try {
+				File gp = dir.getParentFile().getParentFile().getParentFile();
+				Optional<Path> fp = Files.find(gp.toPath(), 3, (p, a) -> p.getFileName().toString().equals(FEATURE_PLUGIN)).findFirst();
+				if (fp.isPresent()) {
+					version = parseProductVersionFromFeature(fp.get().resolve("feature.xml"));
+				}
+			} catch (IOException e) {
+				// do nothing
+			}
+		}
 		return version;
+	}
+
+	private static final Pattern VERSION_MATCH = Pattern.compile("\\s*version=\"([^\"]+)\"");
+
+	private static String parseProductVersionFromFeature(Path path) throws IOException {
+		List<String> lines = Files.readAllLines(path, Charset.defaultCharset());
+
+		Iterator<String> it = lines.iterator();
+		while (it.hasNext()) {
+			if (it.next().contains("<feature")) {
+				break;
+			}
+		}
+		while (it.hasNext()) {
+			Matcher m = VERSION_MATCH.matcher(it.next());
+			if (m.matches()) {
+				return m.group(1);
+			}
+		}
+
+		return null;
 	}
 }
