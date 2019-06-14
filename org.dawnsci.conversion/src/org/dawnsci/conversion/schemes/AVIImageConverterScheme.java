efeee36c73bcace66e93c851f5d501015ca7ab2f
Jacob Filik <jacob.filik@diamond.ac.uk>
2019-06-14 16:45:00 +0100

    Make export to AVI an operation
    Remove AVI conversion
    
    Change-Id: Ifd5f28d8cf140a22728e0089c51eb6a78cc0a930

--- org.dawnsci.conversion/src/org/dawnsci/conversion/schemes/AVIImageConverterScheme.java
+++
@@ -1,9 +0,0 @@
-package org.dawnsci.conversion.schemes;
-
-import org.dawnsci.conversion.converters.AVIImageConverter;
-
-public class AVIImageConverterScheme extends AbstractConversionScheme {
-	public AVIImageConverterScheme() {
-		super(AVIImageConverter.class, " video from image stack", true, 2,3,4,5);
-	}
-}
